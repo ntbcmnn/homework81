4bb4cb0a69c51478cf6c6a4671689ff11aa4cdea
Natalya Botsman <botsmannatashaa@gmail.com>
2024-12-26 13:56:30 +0600

    1. Created link result component 2. Imported needed fields from the global state 3. Displayed the result: used api_url const and added the shortUrl of our link object 4. By click on the shortened url, it redirects user on the originalUrl page

---
+++ frontend/src/Components/LinkResult/LinkResult.tsx
@@ -0,0 +1,32 @@
+import { useAppSelector } from '../../app/hooks.ts';
+import { selectLink, selectLoading } from '../../store/slices/linksSlice.ts';
+import Loader from '../UI/Loader/Loader.tsx';
+import { api_URL } from '../../globalConst.ts';
+
+const LinkResult = () => {
+  const link = useAppSelector(selectLink);
+  const isLoading = useAppSelector(selectLoading);
+
+  return (
+    link && (
+      <>
+        {isLoading ? (
+          <Loader/>
+        ) : (
+          <div className="d-flex justify-content-center align-items-center gap-3 mt-5">
+            <h4 className="m-0 p-0">Your link now looks like this:</h4>
+            <a
+              href={`${link.originalUrl}`}
+              target="_blank"
+              className="text-decoration-none"
+            >
+              {`${api_URL}/${link.shortUrl}`}
+            </a>
+          </div>
+        )}
+      </>
+    )
+  );
+};
+
+export default LinkResult;
