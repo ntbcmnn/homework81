0f00d0ea8c5c7c36ac383c467fcd991298a5b6c5
Natalya Botsman <botsmannatashaa@gmail.com>
2024-12-26 13:46:57 +0600

    1. Imported & used links router 2. Changed url name in the connection

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoDb from "./mongoDb";
 import * as mongoose from "mongoose";
+import linksRouter from "./routers/links";
 
 const app = express();
 const port = 8000;
@@ -10,8 +11,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
+app.use('/links', linksRouter);
+
 const run = async () => {
-    await mongoose.connect('mongodb://localhost/shop');
+    await mongoose.connect('mongodb://localhost/links');
 
     app.listen(port, () => {
         console.log(`Server started on port http://localhost:${port}`);
