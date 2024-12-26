ba28950fc4b5bd5aa9142c516f60baf502c2ceb2
Natalya Botsman <botsmannatashaa@gmail.com>
2024-12-26 14:02:44 +0600

    Displayed Toolbar, LinkForm & LinkResult components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,16 @@
 import './App.css';
+import LinkForm from './Components/LinkForm/LinkForm.tsx';
+import LinkResult from './Components/LinkResult/LinkResult.tsx';
+import Toolbar from './Components/UI/Toolbar/Toolbar.tsx';
 
-const App = () => <></>;
+const App = () => (
+  <>
+    <Toolbar/>
+    <div className="container m-5">
+      <LinkForm/>
+      <LinkResult/>
+    </div>
+  </>
+);
 
 export default App;
